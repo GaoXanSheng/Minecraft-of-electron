7286b980f9587600d4ae0dcad2bc2f6d4290044c
Gaoxusheng <moxiaojulove@foxmail.com>
2024-10-05 10:55:03 +0800

    fix dev port

--- src/main/java/top/yunmouren/electron/Client/Client.java
+++ src/main/java/top/yunmouren/electron/Client/Client.java
@@ -19,8 +19,7 @@ public class Client {
         try {
             Thread.sleep(3000);
             browser.Api.overlapWindows();
-//            browser.NodeJs.start("localhost", browser.BrowserPort);
-            browser.NodeJs.start("localhost",9090);
+            browser.NodeJs.start("localhost", browser.BrowserPort);
         } catch (InterruptedException e) {
             Electron.logger.error(e.getMessage());
         }
