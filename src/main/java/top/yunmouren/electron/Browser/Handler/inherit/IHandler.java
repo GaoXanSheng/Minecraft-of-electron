65e050d0f5fed5714d15ec591c1656f3b08ca2ba
Gaoxusheng <moxiaojulove@foxmail.com>
2024-10-06 18:00:42 +0800

    Update doc, update some methods

--- src/main/java/top/yunmouren/electron/Browser/Handler/inherit/IHandler.java
+++ src/main/java/top/yunmouren/electron/Browser/Handler/inherit/IHandler.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonObject;
 
 /**
  * If you want to handle messages from the web interface, you need to inherit this method <br/>
- * 如果你想处理来自网页界面的消息，你需要继承这个方法 <br/>
  * JsCode... <br/>
  * window.electron.ipcRenderer.invoke('Handler-processorAllocation',{ <br/>
  * 	to:'Minecraft',  <br/>
